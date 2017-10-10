80a803a95751dcfa10b47753415054d13c7b15d3
Shea Levy <shea@shealevy.com>
2017-10-10 12:24:30 -0400

    Partition all-cabal-hashes into case-insensitive-safe components.
    OS X by default has a case-insensitive filesystem, and fetching
    all-cabal-hashes there fails due to a hash mismatch caused by package
    pairs like compactable and Compactable. This partitions the package set
    such that each partition contains no equivalent-up-to-case pairs.
    
    (cherry picked from commit 843e0992ca44be86cd7ca97f337bdd55a91c1655)

--- pkgs/data/misc/hackage/default.nix
+++ pkgs/data/misc/hackage/default.nix
@@ -1,11 +1,33 @@
-{ fetchFromGitHub }:
+{ fetchFromGitHub
+, stdenv
+, pkgconfig
+, libarchive
+, glib
+, # Override this to use a different revision
+  src-spec ?
+    { owner = "commercialhaskell";
+      repo = "all-cabal-hashes";
+      rev = "60443435510c1523ae4596f20595a274531dd485";
+      sha256 = "1k3c0ix5rax92ywrpjxd7cmbzwsgrv03s6dvq6wjm8vljchqg4li";
+    }
+, lib
+}:
 
 # Use builtins.fetchTarball "https://github.com/commercialhaskell/all-cabal-hashes/archive/hackage.tar.gz"
 # instead if you want the latest Hackage automatically at the price of frequent re-downloads.
-
-fetchFromGitHub {
-  owner = "commercialhaskell";
-  repo = "all-cabal-hashes";
-  rev = "60443435510c1523ae4596f20595a274531dd485";
-  sha256 = "1k3c0ix5rax92ywrpjxd7cmbzwsgrv03s6dvq6wjm8vljchqg4li";
-}
+let partition-all-cabal-hashes = stdenv.mkDerivation
+      { name = "partition-all-cabal-hashes";
+        src = ./partition-all-cabal-hashes.c;
+        unpackPhase = "true";
+        buildInputs = [ pkgconfig libarchive glib ];
+        buildPhase =
+          "cc -O3 $(pkg-config --cflags --libs libarchive glib-2.0) $src -o partition-all-cabal-hashes";
+        installPhase =
+          ''
+            mkdir -p $out/bin
+            install -m755 partition-all-cabal-hashes $out/bin
+          '';
+      };
+in fetchFromGitHub (src-spec //
+  { postFetch = "${partition-all-cabal-hashes}/bin/partition-all-cabal-hashes $downloadedFile $out";
+  })
